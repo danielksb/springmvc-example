d363a72032f899b247c7a5aecd6f24a16ed2134c
Daniel <daniel.kaes@web.de>
2013-03-18 22:53:33 +0100

    hibernate: configuration

---
+++ src/main/java/example/springmvc/model/users/impl/UserStorageSQLImpl.java
@@ -0,0 +1,20 @@
+package example.springmvc.model.users.impl;
+
+import example.springmvc.model.users.User;
+import example.springmvc.model.users.UserStorage;
+
+public class UserStorageSQLImpl implements UserStorage {
+
+	@Override
+	public User byId(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveOrUpdate(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
