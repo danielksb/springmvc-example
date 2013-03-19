814b617d691429b7b7443780427d1ccd969b8c42
Daniel <daniel.kaes@web.de>
2013-03-19 22:01:35 +0100

    hibernate: added transaction management

--- src/main/java/example/springmvc/model/blog/impl/BlogEntryStorageSQLImpl.java
+++ src/main/java/example/springmvc/model/blog/impl/BlogEntryStorageSQLImpl.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import example.springmvc.model.blog.BlogEntry;
 import example.springmvc.model.blog.BlogEntryStorage;
@@ -16,34 +17,30 @@ public class BlogEntryStorageSQLImpl implements BlogEntryStorage {
 	private SessionFactory sessionFactory;
 
 	@Override
+	@Transactional(readOnly=true)
 	public BlogEntry byId(String id) {
 		Session session = this.sessionFactory.getCurrentSession();
-		session.beginTransaction();
 		BlogEntry entry = (BlogEntry) session.get(BlogEntry.class, id);
-		session.getTransaction().commit();
 		return entry;
 	}
 
 	@Override
+	@Transactional
 	public void saveOrUpdate(BlogEntry entry) {
 		String id = entry.getId();
 		if (id == null || id.length() == 0) {
 			entry.setId(UUID.randomUUID().toString());
 		}
 		Session session = this.sessionFactory.getCurrentSession();
-		session.beginTransaction();
 		session.saveOrUpdate(entry);
-		session.getTransaction().commit();
 	}
 
 	@Override
+	@Transactional(readOnly=true)
+	@SuppressWarnings("unchecked")
 	public List<BlogEntry> findAll() {
 		Session session = this.sessionFactory.getCurrentSession();
-		session.beginTransaction();
-		@SuppressWarnings("unchecked")
-		List<BlogEntry> list = session.createQuery("from BlogEntry").list();
-		session.getTransaction().commit();
-		return list;
+		return session.createQuery("from BlogEntry").list();
 	}
 
 }
--- src/main/java/example/springmvc/model/users/impl/UserStorageSQLImpl.java
+++ src/main/java/example/springmvc/model/users/impl/UserStorageSQLImpl.java
@@ -3,6 +3,7 @@ package example.springmvc.model.users.impl;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import example.springmvc.model.users.User;
 import example.springmvc.model.users.UserStorage;
@@ -13,19 +14,17 @@ public class UserStorageSQLImpl implements UserStorage {
 	private SessionFactory sessionFactory;
 	
 	@Override
+	@Transactional(readOnly=true)
 	public User byId(String id) {
 		Session session = this.sessionFactory.getCurrentSession();
-		session.beginTransaction();
 		User user = (User) session.get(User.class, id);
-		session.getTransaction().commit();
 		return user;
 	}
 
 	@Override
+	@Transactional
 	public void saveOrUpdate(User user) {
 		Session session = this.sessionFactory.getCurrentSession();
-		session.beginTransaction();
 		session.saveOrUpdate(user);
-		session.getTransaction().commit();
 	}
 }
